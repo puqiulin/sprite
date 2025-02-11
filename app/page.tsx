da22f0f7fbed9ce5899123b592c41aa143ff4613
one0day <puqiulin20@gmail.com>
2025-02-11 16:43:15 +0800

    feat: add image card

--- app/page.tsx
+++ app/page.tsx
@@ -12,8 +12,8 @@ const Home = () => {
       </div>
 
       <div className="mt-20 container mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        {Array.from({ length: 10 }).map((item, i) => (
-          <ImageCard key={i} item={item} />
+        {Array.from({ length: 10 }).map((_, i) => (
+          <ImageCard key={i} />
         ))}
       </div>
     </div>
--- components/image-card.tsx
+++ components/image-card.tsx
@@ -3,11 +3,7 @@ import * as React from "react";
 import { Card } from "@/components/ui/card";
 import sprite from "@/public/sprite.jpg";
 
-type ImageCardProps = {
-  item: any;
-};
-
-const ImageCard: React.FC<ImageCardProps> = ({ item }) => {
+const ImageCard = () => {
   return (
     <Card className="h-[500px] overflow-hidden rounded-lg shadow-sm hover:shadow-lg hover:cursor-pointer transition-shadow duration-300 ease-in-out dark:hover:shadow-white/50">
       <img
