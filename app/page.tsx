41d6c37b50835a5755f0347d0437362a9a766ed9
one0day <puqiulin20@gmail.com>
2025-02-14 17:27:25 +0800

    feat: change text

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,20 @@
 "use client";
 
 import ImageCard from "@/components/image-card";
-import { ModeToggle } from "@/components/mode-toggle";
+// import { ModeToggle } from "@/components/mode-toggle";
 
 const Home = () => {
   return (
     <div className="flex flex-col ">
       <div className="w-full p-4 flex backdrop-blur-lg dark:bg-black/50 fixed justify-between z-50 items-center">
-        <div className="text-6xl font-bold">Hello, Sprite</div>
-        <ModeToggle />
+        <div className="text-6xl font-semibold text-red-300">
+          情人节快乐！小王王～
+        </div>
+        {/* <ModeToggle /> */}
       </div>
 
       <div className="mt-20 container mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        {Array.from({ length: 10 }).map((_, i) => (
+        {Array.from({ length: 6 }).map((_, i) => (
           <ImageCard key={i} />
         ))}
       </div>
