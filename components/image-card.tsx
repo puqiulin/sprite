f3f019ed96e19332cefa18467a3c33284ed3e378
jack <puqiulin@gamil.com>
2025-02-15 12:20:32 +0800

    change png

--- components/image-card.tsx
+++ components/image-card.tsx
@@ -1,13 +1,13 @@
 import * as React from "react";
 
 import { Card } from "@/components/ui/card";
-import sprite from "@/public/sprite.jpg";
+import sprite1 from "@/public/sprite1.png";
 
 const ImageCard = () => {
   return (
     <Card className="h-[500px] overflow-hidden rounded-lg shadow-sm hover:shadow-lg hover:cursor-pointer transition-shadow duration-300 ease-in-out dark:hover:shadow-white/50">
       <img
-        src={sprite.src}
+        src={sprite1.src}
         alt="asd"
         className="w-full h-full object-cover transition-transform duration-300 ease-in-out hover:scale-125"
       />
